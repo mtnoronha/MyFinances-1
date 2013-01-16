1214bb66050063ea98398d5181a6b7c4c4799662
ggdio <ggrdio@gmail.com>
2013-01-16 12:47:22 -0200

    Modificada para funcionar como superclasse abstrata de pessoa

--- myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Pessoa.java
+++ myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Pessoa.java
@@ -5,41 +5,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
 
 @Entity
-public class Pessoa
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Pessoa
 {
 	@Id 
-	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	@GeneratedValue(strategy=GenerationType.TABLE) 
 	@Column(nullable=false)
 	private Long id;
 	private String nome;
-	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
-	private DateTime dataNascimento;
-	@Column(length=10)
-	private String rg;
-	@Column(length=11)
-	private String cpf;
-	@Column(length=14)
-	private String cnpj;
-	@OneToOne
-	@Cascade(value={CascadeType.ALL})
+	@OneToOne @Cascade(value={CascadeType.ALL})
 	private Endereco endereco;
 	private String telefone;
 	private String celular;
 	private String email;
 	
-	public Long getId()
+	public Long getId() 
 	{
 		return id;
 	}
-	public void setId(Long id) 
+	public void setId(Long id)
 	{
 		this.id = id;
 	}
@@ -47,35 +39,11 @@ public class Pessoa
 	{
 		return nome;
 	}
-	public void setNome(String nome)
+	public void setNome(String nome) 
 	{
 		this.nome = nome;
 	}
-	public DateTime getDataNascimento()
-	{
-		return dataNascimento;
-	}
-	public void setDataNascimento(DateTime dataNascimento)
-	{
-		this.dataNascimento = dataNascimento;
-	}
-	public String getRg() 
-	{
-		return rg;
-	}
-	public void setRg(String rg)
-	{
-		this.rg = rg;
-	}
-	public String getCpf() 
-	{
-		return cpf;
-	}
-	public void setCpf(String cpf) 
-	{
-		this.cpf = cpf;
-	}
-	public Endereco getEndereco()
+	public Endereco getEndereco() 
 	{
 		return endereco;
 	}
@@ -87,11 +55,11 @@ public class Pessoa
 	{
 		return telefone;
 	}
-	public void setTelefone(String telefone) 
+	public void setTelefone(String telefone)
 	{
 		this.telefone = telefone;
 	}
-	public String getCelular() 
+	public String getCelular()
 	{
 		return celular;
 	}
@@ -103,7 +71,7 @@ public class Pessoa
 	{
 		return email;
 	}
-	public void setEmail(String email)
+	public void setEmail(String email) 
 	{
 		this.email = email;
 	}
