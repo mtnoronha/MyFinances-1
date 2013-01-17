69350d5ed9bee1df658203021b8bb1af4136bb7d
ggdio <ggrdio@gmail.com>
2013-01-17 10:05:23 -0200

    Remoção do campo Recurso.

--- myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Usuario.java
+++ myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Usuario.java
@@ -2,10 +2,7 @@ package br.com.sourcesphere.myfinances.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.Where;
@@ -13,6 +10,11 @@ import org.joda.time.DateTime;
 
 import br.com.sourcesphere.myfinances.seguranca.Criptografia;
 
+/**
+ * Classe que representa a entidade Usuario
+ * @author Guilherme Dio
+ * @since 1.0
+ */
 @Entity
 @SQLDelete(sql="update usuario set ativo = 0 where id = ?")
 @Where(clause="ativo = 1")
@@ -21,8 +23,6 @@ public class Usuario extends Pessoa
 	@Column(unique=true,nullable=false)
 	private String login;
 	private String senha;
-	@OneToOne @Cascade(value={CascadeType.ALL})
-	private Recurso recurso;
 	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
 	private DateTime dataCadastro;
 	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
@@ -46,14 +46,6 @@ public class Usuario extends Pessoa
 	public void setSenha(String senha) 
 	{
 		this.senha = Criptografia.gerarHash(senha);
-	}
-	public Recurso getRecurso() 
-	{
-		return recurso;
-	}
-	public void setRecurso(Recurso recurso) 
-	{
-		this.recurso = recurso;
 	}
 	public void setDataCadastro(DateTime dataCadastro) 
 	{
