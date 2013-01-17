80405fb2067cee83df0953b3f304a05876e1f0d0
ggdio <ggrdio@gmail.com>
2013-01-17 10:12:32 -0200

    Nova classe para utilidade em equals dinamico

---
+++ myfinances/src/main/java/br/com/sourcesphere/myfinances/util/EqualsUtil.java
@@ -0,0 +1,11 @@
+package br.com.sourcesphere.myfinances.util;
+
+/**
+ * Classe para verificar dinamicamente se objetos são iguais, por reflection
+ * @author Guilherme Dio
+ * @since 1.0
+ */
+public class EqualsUtil 
+{
+	//Implementar
+}
