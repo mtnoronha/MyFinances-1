98153efa763a9ddaf203909f38c7a3db0f875785
ggdio <ggrdio@gmail.com>
2013-01-17 10:04:33 -0200

    Alterações para gerenciar várias pessoas em caso de compromisso de encontro. Adição da pessoa responsável/Usuario.

--- myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Compromisso.java
+++ myfinances/src/main/java/br/com/sourcesphere/myfinances/model/Compromisso.java
@@ -1,5 +1,9 @@
 package br.com.sourcesphere.myfinances.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -7,6 +11,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Cascade;
@@ -14,13 +19,20 @@ import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
 
+/**
+ * Classe que representa a entidade Compromisso
+ * @author Guilherme Dio
+ * @since 1.0
+ */
 @Entity
 public final class Compromisso
 {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) @Column(nullable=false)
 	private Long id;
 	@OneToOne @Cascade(value={CascadeType.ALL})
-	private Pessoa pessoa;
+	private Usuario responsavel;
+	@OneToMany
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
 	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
 	private DateTime data;
 	@OneToOne @Cascade(value={CascadeType.ALL})
@@ -28,50 +40,76 @@ public final class Compromisso
 	@Enumerated(EnumType.ORDINAL)
 	private Prioridade prioridade;
 	
-	public Pessoa getPessoa() 
+	public Long getId() 
 	{
-		return pessoa;
+		return id;
 	}
-	public void setPessoa(Pessoa pessoa)
+	public void setId(Long id)
 	{
-		this.pessoa = pessoa;
+		this.id = id;
+	}
+	public Usuario getResponsavel() 
+	{
+		return responsavel;
+	}
+	public void setResponsavel(Usuario responsavel)
+	{
+		this.responsavel = responsavel;
+	}
+	
+	public Pessoa getPessoa(Integer posicao)
+	{
+		if(posicao >= 0)
+		{
+			if(posicao < this.getPessoas().size())
+			{
+				return this.getPessoas().get(posicao);
+			}
+		}
+		throw new ArrayIndexOutOfBoundsException("A posição da pessoa não existe na lista");
+	}
+	public List<Pessoa> getPessoas()
+	{
+		return this.pessoas;
+	}
+	public void addPessoa(Pessoa pessoa)
+	{
+		if(pessoa != null)
+		{
+			this.pessoas.add(pessoa);
+		}
+	}
+	public void addAllPessoas(Set<Pessoa> pessoas) 
+	{
+		if(pessoas == null) throw new NullPointerException("Lista de pessoas não pode ser nula");
+		for(Pessoa pessoa : pessoas)
+		{
+			this.addPessoa(pessoa);
+		}
 	}
 	
 	public DateTime getData() 
 	{
 		return data;
 	}
-	
 	public void setData(DateTime data) 
 	{
 		this.data = data;
 	}
-	
 	public Endereco getLocal() 
 	{
 		return local;
 	}
-	
-	public void Endereco(Endereco local) 
+	public void setLocal(Endereco local)
 	{
 		this.local = local;
 	}
-	
-	public Prioridade getPrioridade()
+	public Prioridade getPrioridade() 
 	{
 		return prioridade;
 	}
-	
 	public void setPrioridade(Prioridade prioridade) 
 	{
 		this.prioridade = prioridade;
-	}
-
-	@Override
-	public String toString() 
-	{
-		return "Compromisso [id=" + id + ", pessoa=" + pessoa + ", data="
-				+ data + ", local=" + local + ", prioridade=" + prioridade
-				+ "]";
 	}
 }
