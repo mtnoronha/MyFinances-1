734bbf24c2ba044d6671162e4496f96905e79974
ggdio <ggrdio@gmail.com>
2013-01-17 10:12:50 -0200

    Nova classe para utilidade em clone dinamico de objetos

---
+++ myfinances/src/main/java/br/com/sourcesphere/myfinances/util/CloneUtil.java
@@ -0,0 +1,12 @@
+package br.com.sourcesphere.myfinances.util;
+
+
+/**
+ * Classe para clonar objetos dinamicamente por reflection
+ * @author Guilherme Dio
+ * @since 1.0
+ */
+public class CloneUtil
+{
+	//Implementar
+}
